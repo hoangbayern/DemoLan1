aba3200f212df6b81681ec6006d5d3efe9803ce3
bayernkk <hoangdm2002@gmail.com>
2022-04-05 01:16:22 +0700

    first commit

---
+++ learnphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LearnPHP</title>
+</head>
+<body>
+    <h1>
+    <?php
+$txt = 1;
+$hoang = 2432;
+$ting = $txt + $hoang;
+echo "Ket qua la:  " . $ting . " ";
+?>
+    </h1>
+</body>
+</html>
